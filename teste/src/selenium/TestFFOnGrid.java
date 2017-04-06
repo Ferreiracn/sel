985e926ebd10fd9823a983c6ec7de63ef692873f
Ferreira <bernardo.ferreira@accenture.com>
2017-04-06 10:57:36 +0100

    project upload

---
+++ teste/src/selenium/TestFFOnGrid.java
@@ -0,0 +1,32 @@
+package selenium;
+
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeDriverService;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.Test;
+
+
+
+public class TestFFOnGrid {
+	@Test
+	public static void main() throws MalformedURLException {
+		System.setProperty("webdriver.chrome.driver","C:/Users/bernardo.ferreira/Downloads/chromedriver_win32/chromedriver.exe");
+		DesiredCapabilities cap=  DesiredCapabilities.chrome();
+		cap.setPlatform(Platform.WIN10);
+		cap.setBrowserName("chrome");
+		String url= "http://10.111.42.173:5566";
+		WebDriver driver= new ChromeDriver();
+		driver.get(url);
+		driver.get("http://www.google.com");
+		System.out.println(driver.getTitle());
+	}
+	
+	
+}
